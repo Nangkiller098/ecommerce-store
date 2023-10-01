39e831aaf12cfc90e000ee0e800c54bb27f66d05
Nangkiller098 <hsomnang098@gmail.com>
2023-10-02 06:43:12 +0700

    finish

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,27 @@
-const HomePage = () => {
-  return <div> homepage</div>;
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("854d73a6-877f-40c3-80a7-227705e07cd8");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard 
+          data={billboard}
+        />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  )
 };
 
 export default HomePage;
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,13 @@
 const Footer = () => {
   return (
-    <div>
-      <footer className="bg-white border-t">
-        <div className="mx-auto py-10">
-          <p className="text-center text-xs text-black">
-            &copy: 2023 FakeStoreName, Inc, All right reserved
-          </p>
-        </div>
-      </footer>
-    </div>
-  );
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; 2023 Store, Inc. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  )
 };
 
 export default Footer;
